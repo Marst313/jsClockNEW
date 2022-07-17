538afef15bda73eaffbdff62160925850b0767f9
Marst313 <dlegoinyoman1@gmail.com>
2022-07-17 08:28:03 +0700

    menambahkan fitur opaciti in out pada script.js dan style.css

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const time = document.querySelector('.time p');
 const hourA = document.querySelector('.hour .a1');
 const hourA2 = document.querySelector('.hour .a2');
 
-hourA.addEventListener('click', changeTimeTo12);
-
-function changeTimeTo12() {
+hourA.addEventListener('click', function () {
+  time.classList.add('animasi');
+  time.classList.remove('flip-vertical-fwd');
   time.textContent = '2:05:23 PM';
   hourA.style.color = '#00ff97';
   hourA2.style.color = '#8a8a8a';
-}
+});
 
-hourA2.addEventListener('click', changeTimeTo24);
-
-function changeTimeTo24() {
+hourA2.addEventListener('click', function () {
+  time.classList.remove('animasi');
+  time.classList.add('flip-vertical-fwd');
   time.textContent = '14:05:23';
   hourA.style.color = '#8a8a8a';
   hourA2.style.color = '#00ff97';
-}
+});
