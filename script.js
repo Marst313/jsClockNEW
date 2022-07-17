0a777961c5332898314e48e2d27ecf60ce8514c0
Marst313 <dlegoinyoman1@gmail.com>
2022-07-17 21:24:13 +0700

    pada script.js menambahkan pilihan format jam 12Hour atau 24Hour

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ const time = document.querySelector('.time p');
 const hourA = document.querySelector('.hour .a1');
 const hourA2 = document.querySelector('.hour .a2');
 
+var isPaused = false;
+
 // INIT
-window.setInterval(date, 1000);
-function date() {
-  var d = new Date();
-  time.textContent = d.toLocaleTimeString('en-GB');
+function init() {
+  if (!isPaused) {
+    window.setInterval(date, 1000);
+    function date() {
+      var d = new Date();
+      time.textContent = d.toLocaleTimeString(`${isPaused === true ? 'en-US' : 'en-GB'}`);
+    }
+  }
 }
+
+init();
 
 //12HOUR
 hourA.addEventListener('click', function () {
+  isPaused = true;
+  init();
   time.classList.add('animasi');
   time.classList.remove('flip-vertical-fwd');
   hourA.style.color = '#00ff97';
@@ -26,10 +36,18 @@ hourA2.addEventListener('click', function () {
   time.classList.add('flip-vertical-fwd');
   hourA.style.color = '#8a8a8a';
   hourA2.style.color = '#00ff97';
+  isPaused = false;
 });
 // window.setInterval(ut, 1000);
 
 // function ut() {
 //   var d = new Date();
 //   document.querySelector('.time').innerHTML = d.toLocaleTimeString();
+// }
+
+// window.setInterval(date, 1000);
+// function date() {
+//   var d = new Date();
+//   time.textContent = d.toLocaleTimeString('en-US');
+//   isPaused = true;
 // }
